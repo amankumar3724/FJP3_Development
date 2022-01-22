9a8bba1213a227e0e85a0aed259488b07bb48443
amankumar3724 <amankumar3724@gmail.com>
2022-01-22 23:28:49 +0530

    Reference Datatype

---
+++ JS Concepts/ReferenceDT.js
@@ -0,0 +1,13 @@
+let arr = [1,2,3]
+console.log(arr, typeof(arr))
+
+let obj = {
+    name : 'Aman',
+    age : 23
+}
+console.log(obj, typeof(obj))
+
+function greet() {
+    console.log('Hello')
+}
+console.log(greet, typeof(greet))
