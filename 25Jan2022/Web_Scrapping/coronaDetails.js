4bcb377de25f8dc6523596762fb0f74c84c344c9
amankumar3724 <amankumar3724@gmail.com>
2022-01-27 21:52:53 +0530

    commit2

--- 25Jan2022/Web_Scrapping/coronaDetails.js
+++ 25Jan2022/Web_Scrapping/coronaDetails.js
@@ -1,2 +1,18 @@
 const request = require(request)
 const cheerio = require(cheerio)
+
+console.log('Before')
+request("https://www.worldometers.info/coronavirus/",cb)
+function cb(error,response,html){
+    if(error){
+        console.log(error)
+    }
+    else{
+        handleHtml(html)
+    }
+}
+function handleHtml(){
+
+}
+
+console.log('After')
