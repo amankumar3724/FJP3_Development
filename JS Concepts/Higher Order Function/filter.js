6bf44c87482ab0a9c54fae726a9ca39bc0a3b028
amankumar3724 <amankumar3724@gmail.com>
2022-02-01 23:06:54 +0530

    filter.js

--- JS Concepts/Higher Order Function/filter.js
+++ JS Concepts/Higher Order Function/filter.js
@@ -1,3 +1,8 @@
+// Filter----->
+// * Filter returns a new array containing array elements that matches a specified condition
+// if the condition stands true it will filter out those elements and will presemt them in a new 
+//Array
+
 let arr = [2,3,1,4,5,7,8,44,33,55]
 // We need to take out all the even numbers
 let evenNoArr = arr.filter(function(n){
