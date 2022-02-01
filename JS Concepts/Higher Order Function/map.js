53337e960f3d8297d20e56616ed6e6808b2cc162
amankumar3724 <amankumar3724@gmail.com>
2022-02-01 21:48:26 +0530

    Map.js

---
+++ JS Concepts/Higher Order Function/map.js
@@ -0,0 +1,13 @@
+let arr = [2,5,3,9,8]
+let sqarr = arr.map(function square(v){
+    return v*v
+})
+console.log(arr)
+console.log(sqarr)
+
+let nameArr = ['Aman','Ghanshyam','Satyam']
+let nameMapArr = nameArr.map(function name(n){
+    return n
+})
+console.log(nameArr)
+console.log(nameMapArr)
