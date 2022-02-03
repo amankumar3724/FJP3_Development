f1c30ffd475f166a9265a73146d3e5d52defa01b
amankumar3724 <amankumar3724@gmail.com>
2022-02-03 23:05:44 +0530

    espnSrapping

--- espnScrapping/main.js
+++ espnScrapping/main.js
@@ -17,4 +17,24 @@ function extractLink(html){
     let link = anchorElem.attr("href");
     let fullLink = "https://www.espncricinfo.com/" + link;
     console.log(fullLink);
+    getAllMatchLink(fullLink);
+}
+function getAllMatchLink(uri){
+    request(uri, function(error,response,html){
+        if(error){
+            console.log("error");
+        }
+        else{
+            extractAllLink(html);
+        }
+    })
+}
+function extractAllLink(html){
+    let $ = cheerio.load(html);
+    let scoreCardArr = $('a[data-hover="Scorecard"]')
+    for(let i=0 ; i<scoreCardArr.length ; i++){
+        let link = $(scoreCardArr[i]).attr('href')
+        let fullLink = 'https://www.espncricinfo.com/'+link
+        console.log(fullLink)
+ }
 }
