34ff029d46626f8b70ef6a731a44b52504789197
amankumar3724 <amankumar3724@gmail.com>
2022-02-15 23:43:37 +0530

    readingFilesSerially

---
+++ Async_JS/Promises/readingFilesSerially.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+console.log("Before");
+fs.readFile("f1.txt",cb1);
+function cb1(err, data){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(" " + data);
+        fs.readFile("f2.txt",cb2);
+    }
+}
+function cb1(err, data){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(" " + data);
+        fs.readFile("f3.txt",cb3);
+    }
+}
+function cb1(err, data){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(" " + data);
+    }
+}
+console.log("After");
