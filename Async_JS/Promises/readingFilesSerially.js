fbad3dee469924064ae2861cfa9f7a9037221206
amankumar3724 <amankumar3724@gmail.com>
2022-02-17 21:51:57 +0530

    readinfilesserially

--- Async_JS/Promises/readingFilesSerially.js
+++ Async_JS/Promises/readingFilesSerially.js
@@ -1,27 +1,27 @@
-const fs = require("fs");
-console.log("Before");
-fs.readFile("f1.txt",cb1);
-function cb1(err, data){
-    if(err){
-        console.log(err);
-    } else {
-        console.log(" " + data);
-        fs.readFile("f2.txt",cb2);
-    }
+let fs = require("fs");
+
+console.log("before");
+
+let f1p = fs.promises.readFile("f1.txt");
+
+f1p.then(cb).then(cb2).then(cb3)
+
+function cb(data) {
+  console.log("File data -> " + data);
+  let f2p = fs.promises.readFile("f2.txt");
+
+  return f2p;
 }
-function cb1(err, data){
-    if(err){
-        console.log(err);
-    } else {
-        console.log(" " + data);
-        fs.readFile("f3.txt",cb3);
-    }
+
+function cb2(data) {
+  console.log("File data -> " + data);
+  let f3p = fs.promises.readFile("f3.txt");
+
+  return f3p;
 }
-function cb1(err, data){
-    if(err){
-        console.log(err);
-    } else {
-        console.log(" " + data);
-    }
+
+function cb3(data) {
+  console.log("File data -> " + data);
 }
-console.log("After");
+
+console.log("after");
